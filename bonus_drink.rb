ce45d0b3d9c8efd489f05e551e781f3a02bbd81d
Arakaki-Yuji <arakaki@ryukyu-i.co.jp>
2016-08-12 09:29:53 +0900

    define BonusDrink.total_count_for(amount) method.

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,12 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    service_amount = amount / 3
+    if amount === 0 || service_amount === 0
+      return amount
+    end
+
+    rest = amount % 3
+    amount_used_service = amount - rest
+    amount_used_service + total_count_for(service_amount + rest)
   end
 end
